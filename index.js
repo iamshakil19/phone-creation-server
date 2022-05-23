266c682a7dc791a80ce55f78bf8601fe53b32c66
shakilahmed09 <shakilahmed.pure@gmail.com>
2022-05-23 06:31:09 +0600

    parts get api created

--- index.js
+++ index.js
@@ -8,8 +8,33 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 app.use(cors());
 app.use(express.json())
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dgfkb.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+
+        const partsCollection = client.db('assignment12').collection('parts')
+        const Collection = client.db('assignment12').collection('parts')
+
+        // all get api
+        app.get('/parts', async (req, res) => {
+            const parts = await partsCollection.find().toArray()
+            res.send(parts)
+        })
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir)
+
 app.get('/', (req, res) => {
-    res.send('running genius server')
+    res.send('running assignment 12 server')
 })
 app.listen(port, () => {
     console.log("Listening to port", port);
