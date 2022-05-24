1f68549043f4c51b8859ff3b4fce76c813ab711f
shakilahmed09 <shakilahmed.pure@gmail.com>
2022-05-24 07:37:11 +0600

    payment method api created

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const stripe = require('stripe')("sk_test_51L1AeqHfb8VoLvD9wkDKJ6BbEPBkhJtUouDuHb6hPR1jbPBfVjA41sQaCjQAihLzlhjzm6tivmNJ2slGO68s6H7m00K7WEcjF5")
 const app = express()
 const port = process.env.PORT || 5000;
 require('dotenv').config()
@@ -39,6 +40,13 @@ async function run() {
             res.send(reviews)
         })
 
+        app.get('/order/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const singleOrder = await ordersCollection.findOne(query)
+            res.send(singleOrder)
+        })
+
         app.get('/myOrders', async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
@@ -61,6 +69,17 @@ async function run() {
             res.send(result)
         })
 
+        app.post('/create-payment-intent', async(req, res) => {
+            const { price } = req.body
+            const amount = price * 100
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            })
+            res.send({ clientSecret: paymentIntent.client_secret })
+        })
+
         // all delete api
         app.delete('/myOrders/:id', async (req, res) => {
             const id = req.params.id;
@@ -68,6 +87,10 @@ async function run() {
             const result = await ordersCollection.deleteOne(query)
             res.send(result)
         })
+
+        // all put api
+
+        
 
 
     }
